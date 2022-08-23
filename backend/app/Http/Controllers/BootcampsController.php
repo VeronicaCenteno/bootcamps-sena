a192b8c965679ee2fd58a5f5fa9277b0a0beabbd
Your Name <you@example.com>
2022-08-23 11:33:09 -0500

    Rutas rest

---
+++ backend/app/Http/Controllers/BootcampsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BootcampsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo "Aqui se va mostrar proximamente todos los Bootcamps ";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        echo "Aqqui se crea un Bootcamp";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        echo"Mostrar un Bootcamp cuyo id es: $id";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        echo"Aqui se va a atualizar el Bootcamp cuyo id es: $id";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        echo "Aqui se va a elimnar el Bootcamp cuyo id es: $id";
+    }
+}
